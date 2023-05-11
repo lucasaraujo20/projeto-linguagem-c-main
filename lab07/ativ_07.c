e2118cd9a1b57a818ec7dbf6fd3f23bda502e915
Lucas Araújo de Oliveira <127609884+lucasaraujo20@users.noreply.github.com>
2023-05-11 17:06:38 -0300

    ativ_07.c

--- lab07/ativ_07.c
+++ lab07/ativ_07.c
@@ -1,42 +1,37 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.H>
+#include<string.h>
 
+int main (){
 
-int main(){
-
+    int tamanho, i;
     char pl[100];
-    int i,j, receb;
-    
 
-    printf("Digite uma palavra: ");
+    printf("Digite uma frase ou palavra: ");
     fgets(pl,100,stdin);
-    //Ex: perguntar se
+
+    tamanho = strlen(pl);
+
+    for( i = 0; i < tamanho; i++){
 
 
-    receb = strlen(pl);
-
-    for( i = 0; i < receb; i++){
-
-        if( pl[i] == 'r'|| pl[i] == 'R'){
-
-           pl[i] = 'l';
-
-        }
         
+
+        if( pl[i] == 'r' && pl[i+1] == 'r' ||  pl[i] == 'R' && pl[i+1] == 'R'
+
+            pl[i] = 'l';
+            pl[i+1] = '\a';
+
+        } else if( pl[i] == 'r' || pl[i] == 'R'){
+
+            pl[i] = 'l';
+        }
+
+        printf("%c", pl[i]);
     }
-      
-    /*for( i = 0; i < receb; i++){
-
-        if( pl == r){
-
-        }
-    */}
-        
-    
 
     
-    printf("%s \n",pl);
-    
-   
+
+
     return 0;
-}
+}        
+            
